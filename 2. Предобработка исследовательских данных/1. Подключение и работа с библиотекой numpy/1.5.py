f6c0a72ca18f649218ae8e05624ff7af7cb14571
galaxym31doctor <galaxy_m31@mail.ru>
2025-09-05 23:08:20 +0300

    полувыполнено 1.5

---
+++ 2. Предобработка исследовательских данных/1. Подключение и работа с библиотекой numpy/1.5.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+a = np.array([[2, 8],
+              [1, -6]])
+
+b = np.array([[3, 2, 7],
+              [4, 1, 8],
+              [6, 3, 7]])
+
+c = np.array([[4, 3, 2, 7],
+              [6, 1, 1, -2],
+              [7, 5, 8, 1],
+              [9, 5, -3, -5]])
+
+print('Транспонированная матрица а: ', a.reshape(2, 2))
+print('Транспонированная матрица b: ', b.reshape(3, 3))
+print('Транспонированная матрица c: ', c.reshape(4, 4), '\n')
+
+a = np.linalg.inv(a)
+print('Обратная матрица а: ', a)
+b = np.linalg.inv(b)
+print('Обратная матрица b: ', b)
+c = np.linalg.inv(c)
+print('Обратная матрица c: ', c, '\n')
+
+print('Определитель матрицы a: ', np.linalg.det(a))
+print('Определитель матрицы b: ', np.linalg.det(b))
+print('Определитель матрицы c: ', np.linalg.det(c), '\n')

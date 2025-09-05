920ea544472f852e7004db8de42a9f0d884ecb98
galaxym31doctor <galaxy_m31@mail.ru>
2025-09-05 22:48:08 +0300

    Переделано задание 1.1

--- 2. Предобработка исследовательских данных/1. Подключение и работа с библиотекой numpy/1.1.py
+++ 2. Предобработка исследовательских данных/1. Подключение и работа с библиотекой numpy/1.1.py
@@ -1,27 +1,29 @@
 import numpy as np
 import math
 
-a = np.array([[2.3, 5.1, 4.7],
-[3.5, 6.7, 1.5],
-[8.4, 3.1, 9.2]])
+a = np.array([[2.3, 5.1, 4.7], 
+              [3.5, 6.7, 1.5],
+              [8.4, 3.1, 9.2]])
 
 b = np.array([[4.3, 8.1, 6.1],
-[3.7, 6.2, 1.5],
-[2.4, 5.7, 4.7]])
+             [3.7, 1.2, 6.5],
+             [2.4, 5.7, 4.7]])
+size_a = a.shape
+size_b = b.shape
+print("Массив a содержит ", size_a[0],  " строк и ", size_a[1], " столбцов")
+print("Массив b содержит ", size_b[0],  " строк и ", size_b[1], " столбцов\n")
 
+print("Сумма: ", a + b)
+print("Разность: ", a - b)
+print("Произведение: ", a * b)
+print("Отношение: ", a / b, '\n')
 
-print("В массиве b ", b.size, ' строк и ', b[0].size, 'столбцов\n')
+print("a^2: ", a**2, '\n')
+print("b%2: ", b%2, '\n')
 
-print("Сумма массивов: a + b = ", a + b)
-print("Разность массивов: a - b = ", a - b)
-print("Произведение массивов: a * b = ", a * b)
-print("частное массивов: a / b = ", a / b, "\n")
+for i in range(0, 2):
+    for j in range(0, 2):
+        a[i][j] = math.cos(a[i][j])
+        b[i][j] = math.sin(b[i][j])
 
-c = a ** 2
-print("Массив a в квадрате:", с, "\n")
-
-c_cos = math.cos(a)
-s_Sin = math.sin(a)
-
-print("косинус первого массива + синус второго:", c_cos + s_sin)
-
+print("cos(a) + sin(b)", a + b)
